ac61d93f884229dd2ff968f3c106fde9b8ea0cae
root <root@yonglin2.emd.sashq-r.openstack.sas.com>
2019-09-24 15:33:57 +0000

    ensemble memory limit

--- automl/autobench.py
+++ automl/autobench.py
@@ -32,12 +32,6 @@ from runbench import *
 from DateTime import DateTime
 import time
 
-if not sys.warnoptions:
-    import warnings
-    warnings.simplefilter("ignore")
-current_time = DateTime(time.time(), 'US/Eastern')
-
-
 
 numeric_features =[]
 categorical_features =[]
@@ -47,15 +41,22 @@ metalist = glob.glob(dirt+"meta/*csv")
 datalist = remove_dirt(datalist,dirt+'/opentest/')
 metalist = remove_dirt(metalist,dirt+'/meta/')
 print(datalist)
-for im,meta in enumerate(metalist):
-  framework = 'autosklearn'
-  prepart = True
-  ncore = 4
-  dataset = datalist[im]# "uci_bank_marketing_pd"
-  print("\ndataset:\t",dataset)
-  print("\nmetadata information:\t",meta)
-  for foldn in [0,3,10]:
-    foldn = 0#5/7
-    for timeforjob in [900,3600]:
-      runbenchmark(dataset,framework,foldn,ncore,timeforjob,dirt,meta)
 
+if not sys.warnoptions:
+    import warnings
+    warnings.simplefilter("ignore")
+
+for _ in range(2):
+  current_time = DateTime(time.time(), 'US/Eastern')
+  for im,meta in enumerate(metalist):
+    if im ==7:
+      framework = 'autosklearn'
+      prepart = True
+      ncore = 4
+      dataset = datalist[im]# "uci_bank_marketing_pd"
+      print("\ndataset:\t",dataset)
+      print("\nmetadata information:\t",meta)
+      for foldn in [0]:
+        for timeforjob in [900]:
+          runbenchmark(dataset,framework,foldn,ncore,timeforjob,dirt,meta)
+    
