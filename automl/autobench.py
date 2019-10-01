02f1bbeb24254f2ebaa812fcbacf834587282a2b
root <root@yonglin2.emd.sashq-r.openstack.sas.com>
2019-10-01 20:29:55 +0000

    df

--- automl/autobench.py
+++ automl/autobench.py
@@ -19,7 +19,7 @@ from sklearn.metrics import roc_auc_score,accuracy_score,log_loss,f1_score
 from sklearn.model_selection import cross_val_score
 ##################################################
 import json
-
+import jsonpickle
 from sas7bdat import SAS7BDAT
 import pandas as pd
 import os
@@ -34,7 +34,7 @@ import time
 
 numeric_features =[]
 categorical_features =[]
-dirt = '../data/'
+dirt = '/root/data/'
 datalist = glob.glob(dirt+"opentest/*sas7bdat")
 metalist = glob.glob(dirt+"meta/*csv")
 datalist = remove_dirt(datalist,dirt+'/opentest/')
@@ -56,7 +56,7 @@ for im,meta in enumerate(metalist):
       dataset = datalist[im]# "uci_bank_marketing_pd"
       print("\ndataset:\t",dataset)
       print("\nmetadata information:\t",meta)
-      for foldn in [10]:
+      for foldn in [0,3,10]:
         for timeforjob in [900]:
           runbenchmark(dataset,framework,foldn,ncore,timeforjob,dirt,meta,fitmetrics)
     
---
+++ automl/autobenchauc.py
@@ -0,0 +1,62 @@
+import autosklearn.classification
+import sklearn.model_selection
+import sklearn.datasets
+import sklearn.metrics
+###### Read in data
+import pandas as pd
+import numpy as np
+from scipy.sparse import coo_matrix, vstack
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder,OrdinalEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn import preprocessing
+from sklearn.model_selection import KFold
+from sklearn.metrics import roc_auc_score,accuracy_score,log_loss,f1_score
+from sklearn.model_selection import cross_val_score
+##################################################
+import json
+
+from sas7bdat import SAS7BDAT
+import pandas as pd
+import os
+import sys
+import logging
+import optparse
+from utils import *
+from runbench import *
+
+from DateTime import DateTime
+import time
+
+numeric_features =[]
+categorical_features =[]
+dirt = '/root/data/'
+datalist = glob.glob(dirt+"opentest/*sas7bdat")
+metalist = glob.glob(dirt+"meta/*csv")
+datalist = remove_dirt(datalist,dirt+'/opentest/')
+metalist = remove_dirt(metalist,dirt+'/meta/')
+print(datalist)
+fitmetrics = autosklearn.metrics.roc_auc
+if not sys.warnoptions:
+    import warnings
+    warnings.simplefilter("ignore")
+
+for im,meta in enumerate(metalist):
+  current_time = DateTime(time.time(), 'US/Eastern')
+  if im not in [0,1,4]:
+    for _ in range(5):
+      framework = 'autosklearn'
+      current_time = DateTime(time.time(), 'US/Eastern')
+      prepart = True
+      ncore = 4
+      dataset = datalist[im]# "uci_bank_marketing_pd"
+      print("\ndataset:\t",dataset)
+      print("\nmetadata information:\t",meta)
+      for foldn in [10]:
+        for timeforjob in [900]:
+          runbenchmark(dataset,framework,foldn,ncore,timeforjob,dirt,meta,fitmetrics)
+    
