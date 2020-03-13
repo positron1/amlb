af623061ae7cf0d7448348cbb7f5402dce1b092f
zhuygln <zhuygln@gmail.com>
2020-03-13 12:10:50 -0400

    root data path

--- src/automl/autobench_binarytarget.py
+++ src/automl/autobench_binarytarget.py
@@ -31,7 +31,7 @@ if not sys.warnoptions:
 framework = 'autosklearn'
 task = 'bt'  # interval target task
 prep = False  # Data preprocessing with meta data
-dirt = '../../../data/'  # dataset directory
+dirt = '/root/data/'  # dataset directory
 outputdir = './results/'
 task_token = secrets.token_hex(8)  # generate unique token for this run
 #################################################################################
