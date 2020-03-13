49ae26f1cd003fa9ed5f8e16fc00be4d1331b2c2
zhuygln <zhuygln@gmail.com>
2020-03-13 14:02:00 -0400

    meta data path

--- src/automl/utils.py
+++ src/automl/utils.py
@@ -370,11 +370,10 @@ def init(dirt, task, runlist, timelist, foldlist, rep, task_token):
         fitmetrics = autosklearn.metrics.mean_squared_error
     csvdatalist = glob.glob(dirt + taskname+"/*sas7bdat.csv")
     sasdatalist = glob.glob(dirt + taskname+"/*sas7bdat")
-    metalist = glob.glob(dirt +"/tmp_metadata/meta/*csv")
+    metalist = glob.glob(dirt +"/tmp_metadata/*csv")
     csvdatalist = remove_dirt(csvdatalist, dirt + "/"+taskname+"/")
     sasdatalist = remove_dirt(sasdatalist, dirt + "/"+taskname+"/")
-    metalist = remove_dirt(metalist, dirt +"/tmp_metadata/meta/")
-    #dirt = dirt + taskname+"/"
+    metalist = remove_dirt(metalist, dirt +"/tmp_metadata/")
     csvdatalist = [i[:-13] for i in csvdatalist]
     sasdatalist = [i[:-9] for i in sasdatalist]
     metalist =  [i[:-5] for i in metalist]
@@ -407,6 +406,7 @@ def init(dirt, task, runlist, timelist, foldlist, rep, task_token):
         + ".txt",
         "w",
     )
+    dirt = dirt + taskname+"/"
     return dirt, logfile, csvdatalist, sasdatalist, metalist, timestamp, fitmetrics
 
 
